0f0ec0cb0aa93d773aa8e7c85e71a1bbd580503e
QuinnPooch <pucciquinn@gmail.com>
2022-01-31 09:48:06 -0700

    added homeroutes for login and displaying posts

---
+++ controllers/home-routes.js
@@ -0,0 +1,53 @@
+const router = require('express').Router()
+const sequelize = require('../config/connection')
+const { Post, User, Comment } = require('../models')
+
+// route to login
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/')
+        return
+    }
+    res.render('login');
+});
+
+// tentative route to display all posts(and there comments) assuming user is logged in - this will still require session to work porperly.
+router.get('/', (req, res) => {
+    console.log(req.session)
+    Post.findAll({
+        attributes: [
+            'id',
+            'post_url',
+            'title',
+            'created_at',
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(dbPostData => {
+        const posts = dbPostData.map(post => post.get({ plain: true }));
+        res.render('homepage', { 
+          posts,
+          loggedIn: req.session.loggedIn
+        });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+})
+
+
+module.exports = router
