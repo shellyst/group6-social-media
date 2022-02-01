c63ae49fa10d6b241087798ad9031f7cac45e06d
Michelle <mchristine.stone91@gmail.com>
2022-01-31 19:25:53 -0500

    installed and added sessions to server.js file

--- server.js
+++ server.js
@@ -1,22 +1,38 @@
-const express = require('express');
+const express = require("express");
 
 // Import the routes
-const routes = require('./controllers');
+const routes = require("./controllers");
 
 // Import the connection settings
-const connection = require('./config/connection');
+const connection = require("./config/connection");
+
+const session = require("express-session");
+const sequelize = require("./config/connection");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+
+const sess = {
+  secret: "Super secret secret",
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(session(sess));
 
 const app = express();
 const PORT = process.env.PORT || 3002;
 
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
 // Turn on routes
-app.use(routes);
+app.use(require("./controllers/"));
 
 // Turn on connection to DB and server
 // {force: true} forces MySQL to recreate tables if there are association changes with them.
-connection.sync({force:false}).then(() => {
-    app.listen(PORT, () => console.log('App now listening on PORT ' + PORT));
+connection.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log("App now listening on PORT " + PORT));
 });
