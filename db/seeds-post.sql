6386b7f888277da99e9aefb43b8c2809cc5ad2ef
DYoshizawa079 <daniely079@gmail.com>
2022-02-02 10:42:46 -0500

    Updated seed files to have pics of pets

--- db/seeds-post.sql
+++ db/seeds-post.sql
@@ -1,6 +1,10 @@
 USE group6_social_media_db;
 INSERT INTO post (id, author_id, featured_image, content_text )
 VALUES
-    (1, 1, 'https://educaloi.qc.ca/wp-content/uploads/507810382_1200x563.jpg', 'Wheres that grinder!?'),
-    (2, 1, 'https://educaloi.qc.ca/wp-content/uploads/507810382_1200x563.jpg', 'Hello friend!'),
-    (3, 1, 'https://educaloi.qc.ca/wp-content/uploads/507810382_1200x563.jpg', 'Hello Missy!');
+    (1, 1, 'https://i.imgur.com/eLmmpdR.jpeg', 'My cat sneaks into zoo enclosure, finds another cat'),
+    (2, 2, 'https://i.imgur.com/J2HZir3.jpeg', 'Cat facts'),
+    (3, 1, 'https://i.imgur.com/OhmyAPs.jpeg', 'We forgot a small towel on the floor. The cat claimed it in less than 5 minutes.'),
+    (4, 2, 'https://i.imgur.com/qBpMFOs.jpeg', 'My dogs fell asleep playing with a tennis ball.'),
+    (5, 2, 'https://i.imgur.com/xtoLyW2.jpeg', 'Canadian canoeing adventure with dog'),
+    (6, 1, 'https://i.imgur.com/trZxMHq.jpeg', 'You say $2000 Facebook machine, I say $2000 bird warmer.'),
+    (7, 1, 'https://i.imgur.com/67tSocD.jpeg', 'Not sure if he belongs here but my bird is pretty darn cute');
