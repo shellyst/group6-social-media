1d3ec7bc93e33658cde5e44955effb46de981582
Suthan kathir <suthancode2gmail.com>
2022-02-05 10:59:08 -0500

    signup created

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -33,6 +33,24 @@ router.post("/login", (req,res) => {
 })
 // ---------------------
 
+
+// create Signup
+router.post('/signup', (req, res) => {
+  User.create({
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password,
+  })
+  .then(dbUserData => {
+  
+   console.log(dbUserData);
+      
+      res.status(200).end()
+  });
+});
+
+
+
 // logout route
 router.post('/logout', (req, res) => {
   console.log(req.session)
@@ -94,33 +112,6 @@ router.get("/:id", (req, res) => {
     });
 });
 
-// Post a new user.
-router.post("/", (req, res) => {
-  User.create({
-    username: req.body.username,
-    email: req.body.email,
-    password: req.body.password,
-  })
-    // .then((dbUserData) => {
-    //   req.session.save(() => {
-    //     req.session.user_id = dbUserData.id;
-    //     req.session.username = dbUserData.username;
-    //     req.session.loggedIn = true;
-
-    //     res.json(dbUserData);
-    //   });
-    // })
-    // .catch((err) => {
-    //   console.log(err);
-    //   res.status(500).json(err);
-    // });
-
-    .then(dbUserData => res.json(dbUserData))
-        .catch(err => {
-            console.log(err);
-            res.status(500).json(err);
-        });
-});
 
 // Update a user.
 router.put("/:id", (req, res) => {
