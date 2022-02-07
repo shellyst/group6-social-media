de8c8a9aa5dba367dea22f5d5becd94aec628bd1
Michelle <mchristine.stone91@gmail.com>
2022-02-06 23:27:44 -0500

    removed unneccessary code to keep code DRY

--- public/javascript/create-post.js
+++ public/javascript/create-post.js
@@ -1,27 +1,29 @@
 async function newFormHandler(event) {
-    event.preventDefault();
-  
-    const title = document.querySelector('input[name="post-content"]').value;
-    const post_content = document.querySelector('input[name="post-content"]').value;
-  
-    const response = await fetch(`/dashboard/create/publish`, {
-      method: 'POST',
-      body: JSON.stringify({
-        /* title,
-        post_content */
-        featured_image,
-        content_text
-      }),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    });
-  
-    if (response.ok) {
-      document.location.replace('/dashboard');
-    } else {
-      alert(response.statusText);
-    }
+  event.preventDefault();
+
+  const title = document.querySelector('input[name="post-content"]').value;
+  const post_content = document.querySelector(
+    'input[name="post-content"]'
+  ).value;
+
+  const response = await fetch(`/dashboard/create/publish`, {
+    method: "POST",
+    body: JSON.stringify({
+      featured_image,
+      content_text,
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  if (response.ok) {
+    document.location.replace("/dashboard");
+  } else {
+    alert(response.statusText);
   }
-  
-  document.querySelector('.new-post-form').addEventListener('submit', newFormHandler);
+}
+
+document
+  .querySelector(".new-post-form")
+  .addEventListener("submit", newFormHandler);
