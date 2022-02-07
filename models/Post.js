ab4821caf00e330cc695ea9cbff575f247c670f5
Michelle <mchristine.stone91@gmail.com>
2022-02-06 23:22:02 -0500

    cleaned up Models code

--- models/Post.js
+++ models/Post.js
@@ -1,63 +1,45 @@
-const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection');
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
 
 // Create Post Model
 class Post extends Model {}
 
 // Create fields/columns for Post model
-Post.init({
+Post.init(
+  {
     id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        primaryKey: true,
-        autoIncrement: true
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
     },
     author_id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        // Create reference to the User model
-        references: {
-            model: 'user',
-            key: 'id'
-        }
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      // Create reference to the User model
+      references: {
+        model: "user",
+        key: "id",
+      },
     },
-    // Post Type
-    // Used for discerning whether it's a text post, image or video
-    // post_type: {
-    //     type: DataTypes.STRING,
-    //     allowNull: false
-    // },
-    // // Featured image for post
+
+    // Featured image for post
     featured_image: {
-        type: DataTypes.STRING,
-        allowNull: true
+      type: DataTypes.STRING,
+      allowNull: true,
     },
     // Text content of post
     content_text: {
-        type: DataTypes.STRING,
-        allowNull: true
-    }
-    // Link content of post
-    // content_link: {
-    //     type: DataTypes.STRING,
-    //     allowNull: true,
-    //     validate: {
-    //         isURL: true
-    //     }
-    // },
-    // // Video 
-    // video: {
-    //     type: DataTypes.STRING,
-    //     allowNull: true,
-    //     validate: {
-    //         isURL: true
-    //     }
-    // }
-}, {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+  },
+  {
     sequelize,
     freezeTableName: true,
     underscored: true,
-    modelName: 'post'
-});
+    modelName: "post",
+  }
+);
 
 module.exports = Post;
